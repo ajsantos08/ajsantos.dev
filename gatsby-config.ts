c4a916beaa8df85475f9b1ae78af47528d7b65fe
ajsantos08 <arthurjason08@gmail.com>
2025-11-02 11:41:01 -0700

    fix favicons

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,16 +10,29 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [ {
-   resolve: `gatsby-plugin-manifest`,
-   options: {
-     name: `ajsantos.dev`,
-     short_name: `ajsantos.dev`,
-     start_url: `/`,
-     display: `standalone`,
-     icon: `/src/images/favicon/favicon.ico`, 
-   },
- },],
+  plugins: [{
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ajsantos.dev`,
+        short_name: `ajsantos.dev`,
+        start_url: `/`,
+        display: `standalone`,
+        icon: `src/images/favicon/favicon-96x96.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            src: `src/images/favicon/web-app-manifest-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon/web-app-manifest-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+  ],
 }
 
 export default config
